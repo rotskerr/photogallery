e49e848f0ff581dd90975683d4e428b070d0b3e0
Владислав Войчишин <72383276+rotskerr@users.noreply.github.com>
2024-10-21 19:55:29 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,14 +4,16 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDUBtt6Dk2dNuEIkaa-Hn6cACari788LRk",
+  apiKey: "AIzaSyDUBtt6Dk2dNuEIkaa-Hn6cACari788LRk",
   authDomain: "photogallery-c3c17.firebaseapp.com",
   projectId: "photogallery-c3c17",
   storageBucket: "photogallery-c3c17.appspot.com",
   messagingSenderId: "150403057720",
   appId: "1:150403057720:web:03fea67c9cee4cc0963ecd",
+};
 
-let app, auth, storage, db, firestore;
+let app;
+let auth, storage, db, firestore;
 
 try {
   app = initializeApp(firebaseConfig);
